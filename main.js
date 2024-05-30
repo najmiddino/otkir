23c46e9272ef251022a5dfa1618d636d64a67cf8
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-05-30 15:08:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let firstName=prompt('Ismingizni kiriting:')
+let secondName=prompt('Familiyangizni kiriting:')
+let age=prompt('Yoshingizni kiriting:')
+let subject=prompt('Yoqtirgan faningizni kiriting:')
+let adress=prompt('Yashash manzilingizni kiriting:')
+
+let str="Javoblar uchun rahmat"
+
+alert(str)
+
+console.log(firstName)
+console.log(secondName)
+console.log(age)
+console.log(subject)
+console.log(adress)
